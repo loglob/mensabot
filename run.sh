dc007a4ccf346330204d58909e79e27d64204e86
loglob <23386237+loglob@users.noreply.github.com>
2022-07-02 03:29:21 +0200

    Avoid creating empty logfiles

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+LOGFILE=`date +./logs/mensabot_%F_%X.log`
+
 cd "$(dirname "$(realpath $0)")"
 mkdir -p ./logs
-dotnet run 2>&1 | tee `date +./logs/mensabot_%F_%X.log`
+dotnet run 2>&1 | tee "$LOGFILE"
+
+if [ ! -s "$LOGFILE" ]
+then
+	rm "$LOGFILE"
+fi
