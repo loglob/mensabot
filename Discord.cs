eb13826fd671c7fab7ebee7fcae0bd387c931429
loglob <23386237+loglob@users.noreply.github.com>
2024-06-02 21:04:16 +0200

    Use flattened namespace syntax

--- Discord.cs
+++ Discord.cs
@@ -8,64 +8,63 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mensabot
-{
-	static class Discord
-	{
-		public record Embed(string Title, string? Description, EmbedField[] Fields, EmbedImage? Image);
-		public record EmbedField(string Name, string Value, bool Inline = true);
-		public record EmbedImage(Uri url);
+namespace mensabot;
 
-		private static string ToJson(this object data)
+static class Discord
+{
+	public record Embed(string Title, string? Description, EmbedField[] Fields, EmbedImage? Image);
+	public record EmbedField(string Name, string Value, bool Inline = true);
+	public record EmbedImage(Uri url);
+
+	private static string ToJson(this object data)
+	{
+		DefaultContractResolver contractResolver = new DefaultContractResolver {
+			NamingStrategy = new CamelCaseNamingStrategy()
+		};
+
+
+		using(var writer = new StringWriter())
 		{
-			DefaultContractResolver contractResolver = new DefaultContractResolver {
-				NamingStrategy = new CamelCaseNamingStrategy()
+			new JsonSerializer() {
+				ContractResolver = contractResolver,
+				NullValueHandling = NullValueHandling.Ignore
+			}.Serialize(writer, data);
+			writer.Flush();
+
+			return writer.ToString();
+		}
+	}
+
+	private static async Task Post(string webhook, object data)
+	{
+		var json = data.ToJson();
+		var r = await new HttpClient()
+			.PostAsync(webhook, new StringContent(json, Encoding.UTF8, "application/json"));
+
+		if(!r.IsSuccessStatusCode)
+		{
+			Console.WriteLine($"Failed to POST {webhook}, Response:\n{r}");
+			Console.WriteLine($"Tried to send content: {json}");
+		}
+	}
+
+	public static async Task SendEmbed(string webhook, string msg, IEnumerable<Embed> embeds)
+	{
+		bool head = true;
+
+		foreach(var ch in embeds.Chunk(10))
+		{
+			var post = new Dictionary<string,object>{
+				{ "username", "Mensa Bot" },
+				{ "embeds", ch }
 			};
 
+			if(head)
+				post["content"] = msg;
 
-			using(var writer = new StringWriter())
-			{
-				new JsonSerializer() {
-					ContractResolver = contractResolver,
-					NullValueHandling = NullValueHandling.Ignore
-				}.Serialize(writer, data);
-				writer.Flush();
+			await Post(webhook, post);
 
-				return writer.ToString();
-			}
-		}
-
-		private static async Task Post(string webhook, object data)
-		{
-			var json = data.ToJson();
-			var r = await new HttpClient()
-				.PostAsync(webhook, new StringContent(json, Encoding.UTF8, "application/json"));
-
-			if(!r.IsSuccessStatusCode)
-			{
-				Console.WriteLine($"Failed to POST {webhook}, Response:\n{r}");
-				Console.WriteLine($"Tried to send content: {json}");
-			}
-		}
-
-		public static async Task SendEmbed(string webhook, string msg, IEnumerable<Embed> embeds)
-		{
-			bool head = true;
-
-			foreach(var ch in embeds.Chunk(10))
-			{
-				var post = new Dictionary<string,object>{
-					{ "username", "Mensa Bot" },
-					{ "embeds", ch }
-				};
-
-				if(head)
-					post["content"] = msg;
-
-				await Post(webhook, post);
-
-				head = false;
-			}
+			head = false;
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -5,56 +5,55 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace mensabot
-{
-	/// <summary>
-	///  A case-insensitive filter
-	/// </summary>
-	/// <param name="bias"> Sets whether the filter will be full or empty if both lists are null </param>
-	public class Filter(IEnumerable<string>? whitelist, IEnumerable<string>? blacklist, bool bias)
-	{
-		private readonly HashSet<string>? white = whitelist is null ? (bias || blacklist is not null ? null : []) : new(whitelist, StringComparer.OrdinalIgnoreCase);
-		private readonly HashSet<string> black = new(blacklist ?? [], StringComparer.OrdinalIgnoreCase);
+namespace mensabot;
 
-		public bool Allows(string val)
-			=> (white is null || white.Contains(val)) && !black.Contains(val);
+/// <summary>
+///  A case-insensitive filter
+/// </summary>
+/// <param name="bias"> Sets whether the filter will be full or empty if both lists are null </param>
+public class Filter(IEnumerable<string>? whitelist, IEnumerable<string>? blacklist, bool bias)
+{
+	private readonly HashSet<string>? white = whitelist is null ? (bias || blacklist is not null ? null : []) : new(whitelist, StringComparer.OrdinalIgnoreCase);
+	private readonly HashSet<string> black = new(blacklist ?? [], StringComparer.OrdinalIgnoreCase);
+
+	public bool Allows(string val)
+		=> (white is null || white.Contains(val)) && !black.Contains(val);
+}
+
+class Program
+{
+
+	public record ServerEntry
+	(
+		string Webhook,
+		string[]? MenuBlacklist = null,
+		string[]? MenuWhitelist = null,
+		string Message = "**Morgen gibt es:**",
+		string[]? AllergenWhitelist = null,
+		string[]? AllergenBlacklist = null
+	){
+		public readonly Filter MenuFilter = new(MenuWhitelist, MenuBlacklist, true);
+		public readonly Filter AllergenFilter = new(AllergenWhitelist, AllergenBlacklist, false);
 	}
 
-	class Program
+	static async Task MainAsync()
 	{
+		using var f = File.OpenText("./config.json");
+		var essen = await Menu.GetEssenAsync();
 
-		public record ServerEntry
-		(
-			string Webhook,
-			string[]? MenuBlacklist = null,
-			string[]? MenuWhitelist = null,
-			string Message = "**Morgen gibt es:**",
-			string[]? AllergenWhitelist = null,
-			string[]? AllergenBlacklist = null
-		){
-			public readonly Filter MenuFilter = new(MenuWhitelist, MenuBlacklist, true);
-			public readonly Filter AllergenFilter = new(AllergenWhitelist, AllergenBlacklist, false);
-		}
+		if (!essen.Any())
+			return;
 
-		static async Task MainAsync()
+		foreach (var server in new JsonSerializer().Deserialize<ServerEntry[]>(new JsonTextReader(f))!)
 		{
-			using var f = File.OpenText("./config.json");
-			var essen = await Menu.GetEssenAsync();
-
-			if (!essen.Any())
-				return;
-
-			foreach (var server in new JsonSerializer().Deserialize<ServerEntry[]>(new JsonTextReader(f))!)
-			{
-				await Discord.SendEmbed(server.Webhook, server.Message, essen
-					.Where(e => server.MenuFilter.Allows(e.Ausgabe))
-					.Select(e => e.ToEmbed(server.AllergenFilter)));
-			}
+			await Discord.SendEmbed(server.Webhook, server.Message, essen
+				.Where(e => server.MenuFilter.Allows(e.Ausgabe))
+				.Select(e => e.ToEmbed(server.AllergenFilter)));
 		}
+	}
 
-		static void Main(string[] args)
-		{
-			MainAsync().GetAwaiter().GetResult();
-		}
+	static void Main(string[] args)
+	{
+		MainAsync().GetAwaiter().GetResult();
 	}
 }
