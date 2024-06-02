1b4370619b7119833574243cff395742aecc08c9
loglob <23386237+loglob@users.noreply.github.com>
2024-06-02 18:20:43 +0200

    correctly handle more than 10 menu options

--- Discord.cs
+++ Discord.cs
@@ -1,10 +1,11 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using System.Text;
-using System;
+using System.Threading.Tasks;
 
 namespace mensabot
 {
@@ -23,20 +24,35 @@ namespace mensabot
 
 		private static async Task Post(string webhook, object data)
 		{
+			var json = data.ToJson();
 			var r = await new HttpClient()
-				.PostAsync(webhook, new StringContent(data.ToJson(), Encoding.UTF8, "application/json"));
+				.PostAsync(webhook, new StringContent(json, Encoding.UTF8, "application/json"));
 
 			if(!r.IsSuccessStatusCode)
+			{
 				Console.WriteLine($"Failed to POST {webhook}, Response:\n{r}");
+				Console.Write($"Tried to send content: {json}");
+			}
 		}
 
 		public static async Task SendEmbed(string webhook, string msg, IEnumerable<object> embed)
 		{
-			await Post(webhook, new Dictionary<string,object>{
-				{ "content", msg },
-				{ "username", "Mensa Bot" },
-				{ "embeds", embed }
-			});
+			bool head = true;
+
+			foreach(var ch in embed.Chunk(10))
+			{
+				var post = new Dictionary<string,object>{
+					{ "username", "Mensa Bot" },
+					{ "embeds", ch }
+				};
+
+				if(head)
+					post["content"] = msg;
+
+				await Post(webhook, post);
+
+				head = false;
+			}
 		}
 	}
 }
